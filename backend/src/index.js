33b63189f0724823260f7fd360e61c702998da4e
LautaroSalvador <ltrsalvador@gmail.com>
2026-04-04 12:46:11 -0300

    fix cors url

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,9 +5,9 @@ const db = require('./db')
 const start = async () => {
   try {
     // CORS primero, antes que todo
-    await fastify.register(require('@fastify/cors'), {
-      origin: ['http://localhost:3000', 'https://rentcheck-https://rentcheck-ik7k1oj3p-lautarosalvadors-projects.vercel.app']
-    })
+   await fastify.register(require('@fastify/cors'), {
+  origin: ['http://localhost:3000', 'https://rentcheck-ik7k1oj3p-lautarosalvadors-projects.vercel.app']
+})
 
     // Rutas
     fastify.get('/health', async () => {
